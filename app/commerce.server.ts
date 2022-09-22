c3f7df442cdc0dc3088ba17fad4d80ac6aa5dcb4
Ryan Florence <rpflorence@gmail.com>
2022-09-22 15:32:01 -0600

    remove redis client
    we'll be deploying to cloudflare workers so we won't be able to use redis like this

--- app/commerce.server.ts
+++ app/commerce.server.ts
@@ -1,7 +1,4 @@
 import { createShopifyProvider } from "./models/ecommerce-providers/shopify.server";
-import { createSwrRedisCache } from "./models/request-response-caches/swr-redis-cache.server";
-
-import redisClient from "./redis.server";
 
 if (!process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN) {
   throw new Error(
@@ -13,9 +10,6 @@ let commerce = createShopifyProvider({
   shop: process.env.SHOPIFY_STORE!,
   storefrontAccessToken: process.env.SHOPIFY_STOREFRONT_ACCESS_TOKEN,
   maxAgeSeconds: 60,
-  cache: createSwrRedisCache({
-    redisClient,
-  }),
 });
 
 export default commerce;
